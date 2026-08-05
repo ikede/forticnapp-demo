35a1ae0e77c782107bc977085e9f4414b252930b
ikede <31075738+ikede@users.noreply.github.com>
2026-08-05 23:21:42 +0100

    Rename home function to index and refactor ping

--- app.py
+++ app.py
@@ -4,20 +4,18 @@ import subprocess
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return "FortiCNAPP Demo"
 
 @app.route("/ping")
 def ping():
-
     host = request.args.get("host")
 
-    output = subprocess.check_output(
+    result = subprocess.check_output(
         "ping -c 1 " + host,
         shell=True
     )
 
-    return output
+    return result
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+app.run(host="0.0.0.0", port=5000)
